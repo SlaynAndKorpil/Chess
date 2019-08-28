6ec4777ae0c2c04c14432b399aec6c3d67731edb
FelixL <50499590+SlaynAndKorpil@users.noreply.github.com>
2019-08-28 06:42:11 +0200

    Complete console interpreter overhaul. Now works with a system of command objects.

--- src/main/scala/chess/Main.scala
+++ src/main/scala/chess/Main.scala
@@ -9,6 +9,8 @@ package chess
   * @version alpha 0.2
   */
 object Main extends App {
+  println("version 0.3")
+//  new test.test.TestImpl();
   if (args contains "console") chess.console.Run.main(Array())
   else chess.graphics.Run.main(Array())
 }
--- src/main/scala/chess/console/InputInterpreter.scala
+++ src/main/scala/chess/console/InputInterpreter.scala
@@ -1,68 +1,314 @@
 package chess.console
 
-import chess.framework._
+import chess.console.InputInterpreter._
+import chess.framework.BoardStatus.GameResult._
+import chess.framework.BoardStatus.GameStatus._
+import chess.framework.BoardStatus.ResultReason._
 import chess.framework.IOEvents._
 import chess.framework.Input._
+import chess.framework._
 
 import scala.language.postfixOps
 
 /**
-  * An interface between the console and the board
+  * An interface between the console and the board.
+  * It interprets command line input as commands.
+  *
+  * @author Felix Lehner
+  * @version alpha 0.3
   */
-class InputInterpreter extends ChessIO {
+class InputInterpreter extends ChessIO with CommandRegistrator {
   var board: ChessBoard = ChessBoard.classicalBoard
 
+  // registers all commands
+  Commands.register()
+
+  // Stores reactions to acceptations/ rejections of questions.
+  private var approval: Input[_] = _
+  private var reject: Input[_] = _
+
   /**
-    * runs an interpretation of the console and plays the interpreted moves on the [[board]]
+    * Runs an interpretation of the console and plays the interpreted moves on the [[chess.console.InputInterpreter#board board]].
     */
   def run(): Unit = {
     println(board)
     gameLoop()
 
+    @scala.annotation.tailrec
     def gameLoop(): Unit = {
-      println(board.turn.toString.toUpperCase)
       val input = scala.io.StdIn.readLine
 
-      if (input.length >= 4) {
-        if (isValidNotation(input)) {
-          move(Square(input(0), input(1) asDigit), Square(input(2), input(3) asDigit))
-          println(board)
+      if (awaitsApproval)
+        input.toLowerCase match {
+          case "y" => receiveInput(approval)
+          case "n" => receiveInput(reject)
+          case _ => print("This is not an answer to a yes/no question. Please type either \"y\" or \"n\".")
         }
-        else {
-          val inputs = input.splitAt(input.indexOf(' '))
-          val in1 = inputs._1
-          val in2 = inputs._2.tail
-          in1 match {
-            case "resign" =>
-              board.receive(Resign)
-            case "stop" =>
-              println("stopping process...")
-              System.exit(0)
-            case "save" =>
-              println("saving...")
-              save(in2)
-            case "load" => load(in2) match {
-              case Some(errorMessage) => Error error errorMessage.description
-              case None =>
-            }
-            case _ => Error error "wrong input format"
-          }
-        }
+      else parseInput(input) match {
+        case Message(message) =>
+          println(message)
+        case Question(message, approval, reject) =>
+          this.approval = approval
+          this.reject = reject
+          print(message)
+        case Quit(message) =>
+          println(message)
+          return
       }
-      else Error error "wrong input format"
+
       gameLoop()
     }
-
-    def isValidNotation (s: String): Boolean =
-      s(0).isLetter && s(1).isValidInt && s(2).isLetter && s(3).isValidInt && Square(s(0), s(1).asDigit).isValid && Square(s(2), s(3).asDigit).isValid
   }
 
-  def move (from: Square, to: Square): Unit = receiveInput(MoveParams(from, to))
+  /**
+    * Checks if a specific requirement for input is currently needed by the board.
+    * Otherwise returns `false`.
+    */
+  def awaitsApproval: Boolean = chessBoard.gameStatus match {
+    case PromoReq(_) | DrawAcceptanceReq | TakebackAcceptanceReq => true
+    case _ => false
+  }
+
+  /**
+    * This parses the input by either finding the command that corresponds to the first word in the input
+    * and applying the rest of the input as parameters to it, interpreting the input as move command
+    * or as a response to a question.
+    *
+    * @param input The input that comes right from the console.
+    */
+  def parseInput(input: String): CommandResult = {
+    import Commands.MoveCommand._
+
+    val words = input.split(" ")
+    val commandName = words.head.trim.toLowerCase
+
+    if (existsCommand(commandName)) {
+      val command = allCommandBindings(commandName)
+      command(words.filterNot(_.isEmpty).tail.mkString(" "))
+    }
+    else if (isMoveCommand(input)) parseMove(input)
+    else Message(commandErrorMessage)
+  }
 
   override def update(): Unit = println(board)
 
-  chessReactions += {
-    case ShowEnded(result) => println(s"ended with $result")
-    case message => println(message.toString)
+  /**
+    * This stores and organizes all commands.
+    */
+  object Commands {
+    /**
+      * This contains an array of all commands, so it gets easier to register all at once.
+      * When defining new commands, they have to be added here.
+      */
+    def register(): Unit =
+      // registers all commands; all new commands have to be added to this array in order to work properly
+      Array(
+        LoadCommand,
+        SaveCommand,
+        TakebackCommand,
+        HelpCommand,
+        DrawCommand,
+        ResignCommand,
+        ExitCommand,
+        RestartCommand,
+        MoveCommand
+      ) foreach registerCommand
+
+    object LoadCommand extends ConsoleCommand {
+      override val help: String = "Loads a saved game from the given directory."
+      override val description: String = "Load a saved game."
+      override val names: Array[String] = Array("load")
+      override val paramInfo: String = "<file path>"
+
+      override def apply(params: String): Message = load(params) match {
+        case Some(error) => Message("ERROR: " + error.description)
+        case None => Message("Successfully loaded!")
+      }
+    }
+
+    object SaveCommand extends ConsoleCommand {
+      override val help: String = "Saves the current game to a file. When no path is given as a parameter, the last used save directory is chosen."
+      override val description: String = "Save this game."
+      override val names: Array[String] = Array("save", "s", "write", "w")
+      override val paramInfo: String = "<file path>"
+
+      override def apply(params: String): Message = save(params) match {
+        case Some(error) => Message("ERROR: " + error.description)
+        case None => Message("Successfully saved!")
+      }
+    }
+
+    object TakebackCommand extends ConsoleCommand {
+      override val help: String = "Ask your opponent for allowance to take the last move back. Answers can either be y(es) or n(o)."
+      override val description: String = "Take the last move back."
+      override val names: Array[String] = Array("takeback", "t")
+      override val paramInfo: String = noParams
+
+      override def apply(params: String): Question = {
+        receiveInput(TakebackProposal)
+        Question("Do you want a takeback? (Y/N)", TakebackAcceptance, TakebackReject)
+      }
+    }
+
+    object DrawCommand extends ConsoleCommand {
+      override val help: String = "Asks for a draw. When the same position was reached three times or the fifty moves rule applies, a draw by repetition is caused automatically."
+      override val description: String = "Propose a draw."
+      override val names: Array[String] = Array("draw")
+      override val paramInfo: String = noParams
+
+      override def apply(params: String): Question = {
+        receiveInput(DrawOffer)
+        Question("Do you accept a draw? (Y/N)", DrawAcceptance, DrawReject)
+      }
+    }
+
+    object ResignCommand extends ConsoleCommand {
+      override val help: String = "The currently active color resigns the game."
+      override val description: String = "Resign the game."
+      override val names: Array[String] = Array("resign")
+      override val paramInfo: String = noParams
+
+      override def apply(params: String): Message = {
+        receiveInput(Resign)
+        Message(chessBoard.turn + " resigned!")
+      }
+    }
+
+    object RestartCommand extends ConsoleCommand {
+      override val help: String = "Restart the game from the beginning."
+      override val description: String = "Restart the game."
+      override val names: Array[String] = Array("restart")
+      override val paramInfo: String = noParams
+
+      override def apply(params: String): CommandResult = {
+        chessBoard = ChessBoard.classicalBoard
+        Message("Restarted the game.")
+      }
+    }
+
+    object HelpCommand extends ConsoleCommand {
+      override val help: String = "Prints either an overview of all available commands or, when a command name is given as parameter, a more detailed and in-depth description of a command."
+      override val description: String = "Get help."
+      override val names: Array[String] = Array("help")
+      override val paramInfo: String = "[command name]"
+
+      override def apply(params: String): Message = {
+        val helpMessage =
+          if (existsCommand(params)) specificHelp(getCommand(params))
+          else generalHelp
+        Message(helpMessage)
+      }
+
+      def generalHelp: String = {
+        val allHelps = allCommands map (command => {
+          val commandNames = command.names.map(_.toUpperCase).mkString(" | ")
+          val commandParams = command.paramInfo
+          val commandDescription = command.description
+          s" - $commandNames $commandParams : $commandDescription"
+        })
+        allHelps.mkString("===================<HELP>===================\n", "\n", "\n============================================")
+      }
+
+      def specificHelp(command: ConsoleCommand): String = {
+        val names = command.names map (_.toUpperCase)
+        val params =
+          if (command.paramInfo == noParams) "NO PARAMETERS"
+          else command.paramInfo
+        val help = command.help
+        s"${names.head}     $params\n" +
+          "--------------------------------------------\n" +
+          s"$help\n" +
+          s"Aliases: ${names mkString "|"}"
+      }
+    }
+
+    object ExitCommand extends ConsoleCommand {
+      override val help: String = "End your life."
+      override val description: String = "Stop this BS."
+      override val names: Array[String] = Array("exit", "halt", "stop", "die", "end", "shutdown")
+      override val paramInfo: String = noParams
+
+      override def apply(params: String): Quit = Quit("CYA...")
+    }
+
+    object MoveCommand extends ConsoleCommand {
+      override val help: String = "Try to move from the square defined by the first two characters to the one indicated by the two last ones."
+      override val description: String = "Move a piece from one square to another."
+      override val names: Array[String] = Array("move", "m")
+      override val paramInfo: String = "<letter><digit><letter><digit>"
+
+      override def apply(params: String): CommandResult =
+        if (isMoveCommand(params)) parseMove(params)
+        else Message(parameterErrorMessage)
+
+      /**
+        * Tests if a [[String]] is a valid move command of the form `letter|digit|letter|digit`.
+        */
+      def isMoveCommand(command: String): Boolean =
+        command.length >= 4 &&
+          'a' <= command.head && command.head < 'i' &&
+          '0' < command(1) && command(1) < '9' &&
+          'a' <= command(2) && command(2) < 'i' &&
+          '0' < command(3) && command(3) < '9'
+
+      /**
+        * Parses a move command.
+        */
+      // TODO improve behavior
+      def parseMove(move: String): Message = {
+        val from = Square(move.head, move(1).asDigit)
+        val to = Square(move(2), move(3).asDigit)
+        val moveInput = MoveParams(from, to)
+        val moveMessage = s"Moved ${chessBoard(from)} from $from to $to."
+        receiveInput(moveInput)
+        Message(moveMessage)
+      }
+    }
+
   }
+
+  chessReactions += {
+    case ShowEnded(result) =>
+      import DrawResultReason._
+      import WinResultReason._
+      val resultDescription = result match {
+        case BlackWins(_) => "Black wins"
+        case WhiteWins(_) => "White wins"
+        case Draw(_) => "The game is drawn"
+      }
+      val reason = result.reason match {
+        case Mate => "mate"
+        case Resignation => "resignation"
+        case Time => "timer"
+        case Stalemate => "stalemate"
+        case DrawAgreement => "draw agreement"
+        case Repetition => "repetition"
+        case Blocked => "blocked position"
+        case InsufficientMaterial => "insufficient material"
+        case FiftyMovesRule => "fifty moves rule"
+        case rr: ResultReason => rr.toString
+      }
+      println(s"$resultDescription due to $reason.")
+    case ShowPromotion(on) => println(s"To what piece do you want the pawn on $on to promote to? (Q|R|M|B)")
+    case _ =>
+  }
+}
+
+object InputInterpreter {
+  /**
+    * A message that can be printed whenever no correct result can be processed due to wrong input or some other error.
+    * It refers to the `help` command to check out the other commands.
+    */
+  val commandErrorMessage = "Unknown command. Type \"help\" and press 'ENTER' to see all valid commands."
+
+  /**
+    * An error message that shows that the used command failed because it expected different parameters.
+    */
+  val parameterErrorMessage = "Wrong parameters for this option. Try \"help [command name]\"."
+
+  /**
+    * When used as parameter description in a command definition
+    * this shows that the command does not take any parameters.
+    */
+  val noParams = ""
 }
